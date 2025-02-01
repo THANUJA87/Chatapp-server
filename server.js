7fe65e90850ebd965a66f55f0d9afcfdfa480197
THANUJA87 <thanuja.m87@gmail.com>
2025-02-01 23:33:48 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("send_message", (data) => {
+    console.log(`Message from ${data.sender}: ${data.text}`);
+    io.emit("receive_message", data); 
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
