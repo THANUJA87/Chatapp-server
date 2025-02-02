cc8a4bcad21368b63586260e85d2900e3c95fd42
THANUJA87 <thanuja.m87@gmail.com>
2025-02-02 11:04:41 +0530

    second commit

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { Server } = require("socket.io");
 const app = express();
 app.use(cors());
 
+const PORT = process.env.PORT || 5000;
+
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
